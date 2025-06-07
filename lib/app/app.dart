0c56c6c1ff2710cbc48b9c07e2286a10c7d4c7ae
SazarCode <sazardev@gmail.com>
2025-06-07 10:16:11 -0700

    Implement main application structure in app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mediconnect_app/app/routes/app_routes.dart';
+import 'package:mediconnect_app/app/theme/app_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "MediConnect",
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      initialRoute: AppRoutes.home,
+      onGenerateRoute: AppRoutes.generateRoute,
+    );
   }
 }
